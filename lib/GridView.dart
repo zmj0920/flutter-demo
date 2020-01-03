4e076ab74233e60ae4492be5438504e88c2a56a6
zmj0920 <mj960920@163.com>
2020-01-03 22:51:08 +0800

    update

--- lib/GridView.dart
+++ lib/GridView.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'res/listData.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(title: Text('FlutterDemo')),
@@ -17,12 +17,22 @@ class MyApp extends StatelessWidget {
 class LayoutDemo extends StatelessWidget {
   List<Widget> _getListData() {
     List<Widget> list = new List();
-    for (var i = 0; i < 20; i++) {
+    for (var i = 0; i < listData.length; i++) {
       list.add(Container(
         alignment: Alignment.center,
-        child: Text(
-          '这是第$i条数据',
-          style: TextStyle(color: Colors.white, fontSize: 20),
+        child: Column(
+          children: <Widget>[
+            Image.network(
+              listData[i]['imageUrl'],
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 12),
+            Text(
+              listData[i]['title'],
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            )
+          ],
         ),
         color: Colors.blue,
         // height: 400,  //设置高度没有反应
@@ -45,55 +55,18 @@ class LayoutDemo extends StatelessWidget {
       //一行的 Widget 数量
       crossAxisCount: 2,
       //宽度和高度的比例
-      childAspectRatio: 0.7,
+     // childAspectRatio: 0.7,
       //反转
-      reverse: true,
+      //  reverse: true,
       children: this._getListData(),
     );
   }
 }
 
 class LayoutDemo1 extends StatelessWidget {
-  List listData = [
-    {
-      "title": 'Candy Shop',
-      "author": 'Mohamed Chahin',
-      "imageUrl": 'https://www.itying.com/images/flutter/1.png',
-    },
-    {
-      "title": 'Childhood in a picture',
-      "author": 'Google',
-      "imageUrl": 'https://www.itying.com/images/flutter/2.png',
-    },
-    {
-      "title": 'Alibaba Shop',
-      "author": 'Alibaba',
-      "imageUrl": 'https://www.itying.com/images/flutter/3.png',
-    },
-    {
-      "title": 'Candy Shop',
-      "author": 'Mohamed Chahin',
-      "imageUrl": 'https://www.itying.com/images/flutter/4.png',
-    },
-    {
-      "title": 'Tornado',
-      "author": 'Mohamed Chahin',
-      "imageUrl": 'https://www.itying.com/images/flutter/5.png',
-    },
-    {
-      "title": 'Undo',
-      "author": 'Mohamed Chahin',
-      "imageUrl": 'https://www.itying.com/images/flutter/6.png',
-    },
-    {
-      "title": 'white-dragon',
-      "author": 'Mohamed Chahin',
-      "imageUrl": 'https://www.itying.com/images/flutter/7.png',
-    }
-  ];
-
   Widget _getListData(context, index) {
     return Container(
+      padding: EdgeInsets.all(10),
       child: Column(
         children: <Widget>[
           Image.network(listData[index]['imageUrl']),
@@ -101,14 +74,13 @@ class LayoutDemo1 extends StatelessWidget {
           Text(
             listData[index]['title'],
             textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20),
+            style: TextStyle(fontSize: 18),
           )
         ],
       ),
       decoration: BoxDecoration(
           border:
               Border.all(color: Color.fromRGBO(233, 233, 233, 0.9), width: 1)),
-
       // height: 400,  //设置高度没有反应
     );
   }
--- lib/ListView.dart
+++ lib/ListView.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'res/listData.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -66,43 +68,6 @@ class HomeContent1 extends StatelessWidget {
 
 //远程加载列表
 class HomeContent extends StatelessWidget {
-  List listData = [
-    {
-      "title": 'Candy Shop',
-      "author": 'Mohamed Chahin',
-      "imageUrl": 'https://www.itying.com/images/flutter/1.png',
-    },
-    {
-      "title": 'Childhood in a picture',
-      "author": 'Google',
-      "imageUrl": 'https://www.itying.com/images/flutter/2.png',
-    },
-    {
-      "title": 'Alibaba Shop',
-      "author": 'Alibaba',
-      "imageUrl": 'https://www.itying.com/images/flutter/3.png',
-    },
-    {
-      "title": 'Candy Shop',
-      "author": 'Mohamed Chahin',
-      "imageUrl": 'https://www.itying.com/images/flutter/4.png',
-    },
-    {
-      "title": 'Tornado',
-      "author": 'Mohamed Chahin',
-      "imageUrl": 'https://www.itying.com/images/flutter/5.png',
-    },
-    {
-      "title": 'Undo',
-      "author": 'Mohamed Chahin',
-      "imageUrl": 'https://www.itying.com/images/flutter/6.png',
-    },
-    {
-      "title": 'white-dragon',
-      "author": 'Mohamed Chahin',
-      "imageUrl": 'https://www.itying.com/images/flutter/7.png',
-    }
-  ];
   //自定义方法
   Widget _getListData(context, index) {
     return ListTile(
