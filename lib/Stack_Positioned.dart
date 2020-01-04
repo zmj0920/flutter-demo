ee35cef28a0ad39d4174d118eca2ca748da01a8b
zmj0920 <mj960920@163.com>
2020-01-04 17:27:05 +0800

    update Stack Position

--- lib/Stack_Positioned.dart
+++ lib/Stack_Positioned.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(title: Text('FlutterDemo')),
+      appBar: AppBar(title: Text('Positioned')),
       body: LayoutDemo(),
     ));
   }
